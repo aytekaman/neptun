58865c8b317731d00d13c6223c4435fcbcb7512e
aytekaman <aytekaman@gmail.com>
2017-06-14 01:37:13 +0300

    Add project filters to CMake

---
+++ src/neptun/main/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "glm/glm.hpp"
+
+int main()
+{
+    const glm::vec3 a;
+
+    glm::normalize(a);
+
+    std::cout << "neptune renderer." << std::endl;
+
+	return 0;
+}
