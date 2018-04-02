2645dcad76db0bc5cfb3432fc8e138cf00f464d3
Aytek Aman <aytekaman@gmail.com>
2018-04-02 14:37:20 +0300

    Fix travis.yml

--- src/thirdparty/gl3w/include/GL/gl3w.h
+++ src/thirdparty/gl3w/include/GL/gl3w.h
@@ -31,11 +31,7 @@
 #ifndef __gl3w_h_
 #define __gl3w_h_
 
-#if defined(_WIN32) || defined(_WIN64)
 #include "gl3w/include/GL/glcorearb.h"
-#else
-#include <GL/glcorearb.h>
-#endif
 
 #ifndef __gl_h_
 #define __gl_h_
