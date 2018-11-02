462562b99f258f8a52a373d047d9921ef1cf3b19
Alper Şahıstan <alpersahistan@gmail.com>
2018-11-02 03:09:37 +0300

    Prototype for CUDA kernel
    (with correct organisation of files)

---
+++ src/neptun/cuda/raycaster.cuh
@@ -0,0 +1,32 @@
+#include "cuda_runtime.h"
+
+#include "device_launch_parameters.h"
+
+#include "neptun/main/tet_mesh.h"
+#include "neptun/main/ray_tracer.h"
+#include "neptun/main/scene.h"
+
+#define GLM_FORCE_CUDA
+#include "glm/glm.hpp"
+#include "glm/gtc/constants.hpp"
+
+#include <iostream>
+
+__device__
+inline void swapvec2(glm::vec2 &a, glm::vec2 &b)
+{
+	glm::vec2 t = a;
+	a = b;
+	b = t;
+}
+
+__device__
+inline void swap(unsigned int &a, unsigned int &b)
+{
+	unsigned int t = a;
+	a = b;
+	b = t;
+}
+
+__device__
+inline float crossv(const glm::vec2& a, const glm::vec2& b) { return a.x * b.y - a.y * b.x; }
