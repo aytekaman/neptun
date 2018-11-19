233c43721812a098b7f30d5483e970e20bec9ac2
Alper Şahıstan <alpersahistan@gmail.com>
2018-11-20 01:37:51 +0300

    Test for sending/receiving data to/from device

--- src/neptun/cuda/raycaster.cu
+++ src/neptun/cuda/raycaster.cu
@@ -1,18 +1,45 @@
 #include "raycaster.cuh";
+#include <stdio.h>
 
 __global__
-void raycast_kernel()
+void raycast_kernel(/*Scene &a,*/ Ray *rays, int rays_size/*, IntersectionData *output*/)
 {
-    //for (int i = 0; i < rays.size(); i++)
-    if()
-    {
-        IntersectionData i_data;
-        output.push_back(i_data);
-        output[i].hit = scene.tet_mesh->intersect(rays[i], rays[i].source_tet, output[i]);
-    }
+    int i = threadIdx.x;
+    if(i < rays_size)
+    rays[i].dir.x = 111.0f;
 }
+
+/*void copy_to_gpu(const std::vector<Ray> rays, Ray* d_rays)
+{
+    cudaFree(d_rays);
+    cudaMalloc(&d_rays, rays.size() * sizeof(Ray));
+    cudaMemcpy(d_rays, rays.data(), rays.size() * sizeof(Ray), cudaMemcpyHostToDevice);
+}
+
+void copy_from_gpu(const Ray* d_rays, Ray* rays)
+{
+    cudaMemcpy(rays, d_rays, rays.size() * sizeof(Ray), cudaMemcpyDeviceToHost);
+}*/
 
 void ray_caster_gpu(Scene& scene, std::vector<Ray> rays, std::vector<IntersectionData>& output)
 {
-    
+    Ray *d_rays;
+    Ray* c = new Ray[rays.size()];
+    // Allocate space for device copy of Ray
+    cudaMalloc(&d_rays, rays.size() * sizeof(Ray));
+
+    // Copy inputs to device
+    cudaMemcpy(d_rays, rays.data(), rays.size() * sizeof(Ray), cudaMemcpyHostToDevice);
+
+    // Launch add() kernel on GPU
+    raycast_kernel<<<1, 1024>>> (d_rays, rays.size()/*, nullptr*/);
+
+    // Copy result back to host
+    cudaMemcpy(c, d_rays, rays.size() * sizeof(Ray), cudaMemcpyDeviceToHost);
+    // Cleanup
+    cudaFree(d_rays);
+
+    printf("%lf", c[1023].dir.x);
+
+    delete[] c;
 }
