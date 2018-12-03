efe8c9ccdcb87f41fc7014ae23bd7d7098554104
Alper Şahıstan <alpersahistan@gmail.com>
2018-12-03 20:37:37 +0300

    Tests for checkerboard screen

--- src/neptun/cuda/raycaster.cu
+++ src/neptun/cuda/raycaster.cu
@@ -2,11 +2,16 @@
 #include <stdio.h>
 
 __global__
-void raycast_kernel(/*Scene &a,*/ Ray *rays, int rays_size/*, IntersectionData *output*/)
+void raycast_kernel(/*Scene &a,*/ Ray *rays, int rays_size, IntersectionData *output)
 {
     int i = threadIdx.x;
-    if(i < rays_size)
-    rays[i].dir.x = 111.0f;
+    if (i < rays_size)
+    {
+        if( i % 2)
+            output[i].hit = true;
+        else
+            output[i].hit = false;
+    }
 }
 
 /*void copy_to_gpu(const std::vector<Ray> rays, Ray* d_rays)
@@ -24,22 +29,31 @@ void copy_from_gpu(const Ray* d_rays, Ray* rays)
 void ray_caster_gpu(Scene& scene, std::vector<Ray> rays, std::vector<IntersectionData>& output)
 {
     Ray *d_rays;
-    Ray* c = new Ray[rays.size()];
+    IntersectionData* c = new IntersectionData[rays.size()];
+
+    IntersectionData *d_intersectdata;
+    
     // Allocate space for device copy of Ray
     cudaMalloc(&d_rays, rays.size() * sizeof(Ray));
+    cudaMalloc(&d_intersectdata, rays.size() * sizeof(IntersectionData));
 
     // Copy inputs to device
     cudaMemcpy(d_rays, rays.data(), rays.size() * sizeof(Ray), cudaMemcpyHostToDevice);
 
     // Launch add() kernel on GPU
-    raycast_kernel<<<1, 1024>>> (d_rays, rays.size()/*, nullptr*/);
+    raycast_kernel<<<1, rays.size()>>> (d_rays, rays.size(), d_intersectdata);
 
     // Copy result back to host
-    cudaMemcpy(c, d_rays, rays.size() * sizeof(Ray), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, d_intersectdata, rays.size() * sizeof(IntersectionData), cudaMemcpyDeviceToHost);
+    //cudaMemcpy(&output, d_intersectdata, rays.size() * sizeof(IntersectionData), cudaMemcpyDeviceToHost);
+
     // Cleanup
     cudaFree(d_rays);
+    cudaFree(d_intersectdata);
 
-    printf("%lf", c[1023].dir.x);
+    printf("%d", c[1020].hit);
+    printf("%d", c[1021].hit);
+    output.insert(output.begin(), c, c + rays.size());
 
-    delete[] c;
+    //delete[] c;
 }
