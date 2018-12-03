d2999c9cd8741309e2fd4a3bb7b565877faf75b4
Alper Şahıstan <alpersahistan@gmail.com>
2018-12-03 20:37:37 +0300

    Tests for checkerboard screen

--- src/neptun/main/ray_tracer.cpp
+++ src/neptun/main/ray_tracer.cpp
@@ -399,8 +399,9 @@ void RayTracer::Raytrace_worker2(Scene& scene, SourceTet source_tet, int thread_
             {
 
                 color = glm::vec3();
+                color = glm::vec3(1.0f, 0.0f, 1.0f);
 
-                for (int light_idx = 0; light_idx < lightInfos.size(); light_idx++)
+                /*for (int light_idx = 0; light_idx < lightInfos.size(); light_idx++)
                 {
                     Ray shadow_ray(intersect_data[i + j * m_resolution.x].position,
                         glm::normalize(lightInfos[light_idx].pos - intersect_data[i + j * m_resolution.x].position));
@@ -409,7 +410,7 @@ void RayTracer::Raytrace_worker2(Scene& scene, SourceTet source_tet, int thread_
                         float diffuse = glm::clamp(glm::dot(intersect_data[i + j * m_resolution.x].normal, to_light), 0.0f, 1.0f);
                         color += lightInfos[light_idx].color * diffuse * lightInfos[light_idx].intensity;
                     }
-                }
+                }*/
 
             }
             else
