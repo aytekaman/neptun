c2bda9146a051ab55ca0609a9199fc30f7cf8368
Alper Şahıstan <alpersahistan@gmail.com>
2018-12-24 18:15:55 +0300

    Raycast_16 method with minor indexing bug

--- src/neptun/cuda/raycaster.cu
+++ src/neptun/cuda/raycaster.cu
@@ -126,6 +126,153 @@ void raycast_32(Ray *rays, int rays_size, glm::vec3* d_points, TetMesh32::Tet32*
     }
 }
 
+__global__
+void raycast_16(Ray *rays, int rays_size, glm::vec3* d_points, TetMesh16::Tet16* d_tets, ConstrainedFace* d_cons_faces, Face* d_faces, IntersectionData *output)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < rays_size)
+    {
+        Ray ray = rays[i];
+        unsigned int id[4];
+        glm::vec2 p[4];
+
+        const float sign = copysignf(1.0f, ray.dir.z);
+
+        const float a = -1.0f / (sign + ray.dir.z);
+        const float b = ray.dir.x * ray.dir.y * a;
+
+        const glm::vec3 right(1.0f + sign * ray.dir.x * ray.dir.x * a, sign * b, -sign * ray.dir.x);
+        const glm::vec3 up(b, sign + ray.dir.y * ray.dir.y * a, -ray.dir.y);
+
+        int index;
+
+        for (int i = 0; i < 4; i++)
+        {
+            id[i] = ray.source_tet.v[i];
+            const glm::vec3 point = d_points[id[i]] - ray.origin;
+            p[i].x = glm::dot(right, point);
+            p[i].y = glm::dot(up, point);
+        }
+
+        if (p[2].x * p[1].y <= p[2].y * p[1].x && p[1].x * p[3].y <= p[1].y * p[3].x && p[3].x * p[2].y <= p[3].y * p[2].x)
+        {
+            index = ray.source_tet.n[0];
+            id[0] = id[3];
+            p[0] = p[3];
+        }
+        else if (p[2].x * p[3].y <= p[2].y * p[3].x && p[3].x * p[0].y <= p[3].y * p[0].x && p[0].x * p[2].y <= p[0].y * p[2].x)
+        {
+            index = ray.source_tet.n[1];
+            id[1] = id[3];
+            p[1] = p[3];
+        }
+        else if (p[0].x * p[3].y <= p[0].y * p[3].x && p[3].x * p[1].y <= p[3].y * p[1].x && p[1].x * p[0].y <= p[1].y * p[0].x)
+        {
+            index = ray.source_tet.n[2];
+            id[2] = id[3];
+            p[2] = p[3];
+        }
+        else if (p[0].x * p[1].y <= p[0].y * p[1].x && p[1].x * p[2].y <= p[1].y * p[2].x && p[2].x * p[0].y <= p[2].y * p[0].x)
+        {
+            swap(id[0], id[1]);
+            swapvec2(p[0], p[1]);
+
+            index = ray.source_tet.n[3];
+        }
+        else
+            output[i].hit = false;
+
+        int nx = ray.source_tet.idx;
+
+        while (index >= 0)
+        {
+            id[3] = d_tets[index].x ^ id[0] ^ id[1] ^ id[2];
+            const glm::vec3 newPoint = d_points[id[3]] - ray.origin;
+
+            p[3].x = glm::dot(right, newPoint);
+            p[3].y = glm::dot(up, newPoint);
+
+            const int idx = (id[3] > id[0]) + (id[3] > id[1]) + (id[3] > id[2]);
+
+            if (idx != 0)
+                nx ^= d_tets[index].n[idx - 1];
+
+            if (p[3].x * p[0].y < p[3].y * p[0].x) // copysignf here?
+            {
+                if (p[3].x * p[2].y >= p[3].y * p[2].x)
+                {
+                    const int idx2 = (id[1] > id[0]) + (id[1] > id[2]) + (id[1] > id[3]);
+
+                    if (idx2 != 0)
+                        nx ^= d_tets[index].n[idx2 - 1];
+
+                    id[1] = id[3];
+                    p[1] = p[3];
+                }
+                else
+                {
+                    const int idx2 = (id[0] > id[1]) + (id[0] > id[2]) + (id[0] > id[3]);
+
+                    if (idx2 != 0)
+                        nx ^= d_tets[index].n[idx2 - 1];
+
+                    id[0] = id[3];
+                    p[0] = p[3];
+                }
+            }
+            else if (p[3].x * p[1].y < p[3].y * p[1].x)
+            {
+                const int idx2 = (id[2] > id[0]) + (id[2] > id[1]) + (id[2] > id[3]);
+
+                if (idx2 != 0)
+                    nx ^= d_tets[index].n[idx2 - 1];
+
+                id[2] = id[3];
+                p[2] = p[3];
+            }
+            else
+            {
+                const int idx2 = (id[0] > id[1]) + (id[0] > id[2]) + (id[0] > id[3]);
+
+                if (idx2 != 0)
+                    nx ^= d_tets[index].n[idx2 - 1];
+
+                id[0] = id[3];
+                p[0] = p[3];
+            }
+
+            swap(nx, index);
+        }
+
+        if (index != -1)
+        {
+            index = (index & 0x7FFFFFFF);
+            const Face& face = d_faces[d_cons_faces[index].face_idx];
+
+            const glm::vec3 *v = face.vertices;
+            const glm::vec3 *n = face.normals;
+            const glm::vec2 *t = face.uvs;
+
+            const glm::vec3 e1 = v[1] - v[0];
+            const glm::vec3 e2 = v[2] - v[0];
+            const glm::vec3 s = ray.origin - v[0];
+            const glm::vec3 q = glm::cross(s, e1);
+            const glm::vec3 p = glm::cross(ray.dir, e2);
+            const float f = 1.0f / glm::dot(e1, p);
+            const glm::vec2 bary(f * glm::dot(s, p), f * glm::dot(ray.dir, q));
+            /*output[i].position = ray.origin + f * glm::dot(e2, q) * ray.dir;
+            output[i].normal = bary.x * n[1] + bary.y * n[2] + (1 - bary.x - bary.y) * n[0];
+            output[i].uv = bary.x * t[1] + bary.y * t[2] + (1 - bary.x - bary.y) * t[0];
+            output[i].tet_idx = d_cons_faces[index].tet_idx;
+            output[i].neighbor_tet_idx = d_cons_faces[index].other_tet_idx;*/
+
+            output[i].hit = true;
+        }
+        else
+            output[i].hit = false;
+    }
+}
+
 void copy_to_gpu(TetMesh& tet_mesh)
 {
     cudaFree(d_points);
@@ -149,7 +296,7 @@ void copy_to_gpu_32(TetMesh32& tet_mesh)
 
     cudaFree(d_tets32);
     cudaMalloc(&d_tets32, tet_mesh.m_tets.size() * sizeof(TetMesh32::Tet32));
-    cudaMemcpy(d_tets32, (tet_mesh).m_tet32s, tet_mesh.m_tets.size() * sizeof(TetMesh32::Tet32), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_tets32, tet_mesh.m_tet32s, tet_mesh.m_tets.size() * sizeof(TetMesh32::Tet32), cudaMemcpyHostToDevice);
     
     print_cuda_error("CUDA copy error32");
 }
@@ -171,7 +318,7 @@ void copy_to_gpu_16(TetMesh16& tet_mesh)
 
     cudaFree(d_tets16);
     cudaMalloc(&d_tets16, tet_mesh.m_tets.size() * sizeof(TetMesh16::Tet16));
-    cudaMemcpy(d_tets16, (tet_mesh).m_tet16s, tet_mesh.m_tets.size() * sizeof(TetMesh16::Tet16), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_tets16, tet_mesh.m_tet16s, tet_mesh.m_tets.size() * sizeof(TetMesh16::Tet16), cudaMemcpyHostToDevice);
 
     print_cuda_error("CUDA copy error16");
 }
@@ -202,21 +349,22 @@ void ray_caster_gpu(Ray* rays, unsigned int rays_size, IntersectionData* output)
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     copy_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1e3;
-    printf("Copy time of Rays to GPU: %f ms\n ", copy_time);
+    //printf("Copy time of Rays to GPU: %f ms\n ", copy_time);
 
 
     // Launch kernel on GPU
     int t = 512;
     start = std::chrono::steady_clock::now();
 
-    raycast_32<<< rays_size / t, t >>>(d_rays, rays_size, d_points, d_tets32, d_cons_faces, d_faces, d_intersectdata);
+    //raycast_32<<< rays_size / t, t >>>(d_rays, rays_size, d_points, d_tets32, d_cons_faces, d_faces, d_intersectdata);
+    raycast_16 <<< rays_size / t, t >> > (d_rays, rays_size, d_points, d_tets16, d_cons_faces, d_faces, d_intersectdata);
     cudaDeviceSynchronize();
-    /*error = cudaGetLastError();
-    printf("CUDA error2: %s\n", cudaGetErrorString(error));*/
+
+    print_cuda_error("kernel error");
 
     end = std::chrono::steady_clock::now();
     kernel_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1e3;
-    printf("Kernel time %f ms\n ", kernel_time);
+    //printf("Kernel time %f ms\n ", kernel_time);
 
     // Copy result back to host
     cudaMemcpy(output, d_intersectdata, rays_size * sizeof(IntersectionData), cudaMemcpyDeviceToHost);
